fbccc7c522c32299784c70f14ca8ad90dd79f442
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-07-29 08:42:38 -0400

    Implement basic auth

--- cmd/tracer-heroku/main.go
+++ cmd/tracer-heroku/main.go
@@ -29,6 +29,9 @@ func main() {
 	dburl := envflag.String("DATABASE_URL", "", "URL for the database")
 	port := envflag.Int("PORT", 0, "Port to listen on")
 	host := envflag.String("HOST", "0.0.0.0", "Host to listen on")
+	httpUsers := envflag.String("HTTP_USERS", "", "Credentials for web/HTTP access, format user:pass,user2:pass2")
+	//grpcKeys := envflag.String("GRPC_KEYS", "babababa,cdcdcdcdcd", "Keys for https://github.com/lstoll/grpce/tree/master/handshakeauth")
+
 	envflag.Parse()
 	fTemplate := flag.String("t", "", "The `directory` containing the UI code")
 	flag.Parse()
@@ -61,21 +64,47 @@ func main() {
 
 	srv := &server.Server{Storage: storage}
 
+	// Auther
+	auth := func(fn http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if len(*httpUsers) == 0 {
+				fn(w, r)
+				return
+			}
+			user, pass, _ := r.BasicAuth()
+			authed := false
+			for _, u := range strings.Split(*httpUsers, ",") {
+				sp := strings.Split(u, ":")
+				if user == sp[0] && pass == sp[1] {
+					authed = true
+				}
+			}
+			if !authed {
+				w.Header().Set("WWW-Authenticate", "Basic")
+				http.Error(w, "Unauthorized.", 401)
+				return
+			}
+			fn(w, r)
+		}
+	}
+
 	mux := http.NewServeMux()
+	authedMux := http.NewServeMux()
 
 	// Static UI content
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	authedMux.HandleFunc("/", auth(func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.URL.Path, ".") {
 			http.ServeFile(w, r, filepath.Join(*fTemplate, r.URL.Path))
 			return
 		}
 		http.ServeFile(w, r, filepath.Join(*fTemplate, "index.html"))
-	})
+	}))
 
 	// HTTP Handlers
-	thttp.New(srv, "", mux)
-	zipkinhttp.New(srv, "", mux)
+	thttp.New(srv, "", authedMux)
+	zipkinhttp.New(srv, "", authedMux)
 
+	// set up gRPC
 	grpclis, wsh := wsnet.HandlerWithKeepalive(20 * time.Second)
 	mux.Handle("/grpc", wsh)
 	s := grpc.NewServer()
@@ -87,6 +116,9 @@ func main() {
 			log.Fatalf("Error starting gRPC server: %q", err)
 		}
 	}()
+
+	// Bind the authed mux last
+	mux.HandleFunc("/", auth(authedMux.ServeHTTP))
 
 	// Run it up
 	hl, err := net.Listen("tcp", *host+":"+strconv.Itoa(*port))
